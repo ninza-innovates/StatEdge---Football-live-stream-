3dcfd5665bdcd953977e2fb777602f9410529a28
ninza-innovates <zkctuepf101@gmx.com>
2025-11-06 11:11:45 +0100

    Update the version Co-authored-by: ninza-innovates <zkctuepf101@gmx.com>

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import Stripe from 'https://esm.sh/stripe@14.21.0';
 
 const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') as string, {
-  apiVersion: '2025-08-27.basil', //update the stripe api version
+  apiVersion: '2025-08-27.basil',
 });
 
 const cryptoProvider = Stripe.createSubtleCryptoProvider();
